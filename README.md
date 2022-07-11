2792137ea5eb72eaac90600308464cb1da97dddd
kazukinakama <nakama.kazuki1215@gmail.com>
2022-07-11 23:19:18 +0900

    express-openapi-validatorを設定

--- README.md
+++ README.md
@@ -14,3 +14,8 @@ docker compose up -d
 ```
 npx @stoplight/spectral-cli lint ./docs/openapi/api.yml --ruleset ./docs/openapi/.spectral.yml
 ```
+
+### OpenAPI Validator
+```
+npx --yes swagger-cli bundle --dereference -o specs/merged_api.yaml -t yaml ../../docs/api.yaml && npx --yes openapi-typescript specs/merged_api.yaml --output specs/request_response_schema.ts
+```
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,43 @@
 import express from 'express'
+import * as OpenApiValidator from 'express-openapi-validator'
 
 const app = express()
 const port = process.env.PORT || 80
+const prefix = '/api'
 
-app.get('/api', (req, res) => res.send('Hello World!'))
+app.use(express.json())
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Credentials', 'true')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Authorization,Content-Type'
+    )
+    next()
+  }
+)
+
+app.get(prefix, (req, res) => res.send('Hello World!'))
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: '../specs/merged_api.yml',
+    validateRequests: true,
+    validateResponses: true,
+  }),
+)
+
+app.use((req: express.Request, res: express.Response) => {
+  res.status(404).json({ message: 'Not Found'})
+})
+
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  })
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
